06469d10bf081430d2bad1069dc73fe64f759da1
KnightWebnApps <45805254+KnightWebnApps@users.noreply.github.com>
2022-04-11 12:25:50 -0400

    feat: added netlify identity and cms

---
+++ src/routes/admin/index.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+  <title>Content Manager</title>
+</svelte:head>
+
+
+<script>
+  import { onMount } from 'svelte';
+  onMount(async() => {
+    const CMS = await import('netlify-cms-app/dist/netlify-cms-app')
+    if(window){
+      CMS.init()
+    }
+  })
+</script>
